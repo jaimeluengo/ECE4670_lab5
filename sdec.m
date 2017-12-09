419578848f06cf51cfd0901a7603fd85d69dd685
Joao Pedro Carvao <jc2697@users.noreply.github.com>
2017-12-09 14:03:13 -0500

    fix synchronization issue on audio1
    FoM = 0.996695638788493

--- sdec.m
+++ sdec.m
@@ -1,67 +1,53 @@
-function outbits = dec(y,L1)
-P = 0.00125*4820/4000;
-n_plus = 120;
-n_low=112;
-n_high=708;
-L = 9641;
-num_bits = 2e5;
+function outbits = sdec()
+    y = audioread('rx.wav');
+    P = 0.00125*4820/4000;
+    n_plus = 120;
+    n_low = 112;
+    L = 9641;
+    num_bits = 2e5;
 
-m = 3;
-t = 2;
-
-% estimate c threshold 
-c = y(1:11)' * y(1:11) + y(end - 10: end)' * y(end - 10: end) * 30 / 20;
-
-% for i=1:length(y)
-%     if(abs(y(i))>0.005);
-%         break
-%     end
-% end
-% L1=i;
-% % estimate L1
-s = 0;
-k = 0;
-while s <= c
-    s = 0;
-    k = k + 1;
-    for i = 1:m
-        if k+1-i < 1
+    square = y.^2;
+    W = 10;
+    s = movmean(square, W);
+    len_y = length(y);
+    thresh = 1e-6;
+    tau = -1;
+    for k = 1:len_y
+        if (abs(s(k))>thresh)
+            start = k-tau;  
             break;
         end
-        s = s+(y(k+1-i))^2;
     end
-    s = s/m;
-end
-L1 = k-t;
+    L1 = start;
 
 
-y0 = y(L1+n_plus+1: L1+n_plus+L);
-y0 = fft(y0)/sqrt(L);
-y1 = y0(113:4113); % mmaybe change to 4112
-
-gain = abs(y1/sqrt(2*P));
-bits = [];
-
-counter = 0;
-i=1;
-while (counter < num_bits)
-    y0 = y(L1+n_plus+(n_plus+L)*i+1: L1+n_plus+L+(n_plus+L)*i);
+    y0 = y(L1+n_plus+1: L1+n_plus+L);
     y0 = fft(y0)/sqrt(L);
-    y1 = y0(n_low+2:n_low+4001);
-    z = conj(y1).*y1;
-    for k= 1:4000
-        if abs(y1(k)) >= sqrt(2*P)/2*gain(k);
-        %if z(k) >= sqrt(2*P)/2*gain(k);
-            bits = [bits ; 1];
-        else
-            bits = [bits; 0];
+    y1 = y0(113:4113);
+
+    gain = abs(y1/sqrt(2*P));
+    bits = [];
+
+    counter = 0;
+    i=1;
+    while (counter < num_bits)
+        y0 = y(L1+n_plus+(n_plus+L)*i+1: L1+n_plus+L+(n_plus+L)*i);
+        y0 = fft(y0)/sqrt(L);
+        y1 = y0(n_low+2:n_low+4001);
+        z = conj(y1).*y1;
+        for k= 1:4000
+            if abs(y1(k)) >= sqrt(2*P)/2*gain(k)
+            %if z(k) >= sqrt(2*P)/2*gain(k);
+                bits = [bits ; 1];
+            else
+                bits = [bits; 0];
+            end
+            counter = counter+1;
+            if counter >= num_bits
+                break;
+            end
         end
-        counter = counter+1;
-        if counter >= num_bits
-            break;
-        end
+        i= i+1;
     end
-    i= i+1;
-end
-outbits = bits;
+    outbits = bits;
 return
